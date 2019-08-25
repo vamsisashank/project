6c87c9297789901952e3cc2d390150ed2d36fc04
vamsi sashank kotagiri <vamsi.kotagiri@egen.solutions>
2019-08-25 14:54:24 -0500

    adding actions section

--- javascript/index.js
+++ javascript/index.js
@@ -81,9 +81,69 @@ tbdy.setAttribute('class', 'data-table-data-body');
   }
 };
 
-const addColumn = () => {
-  var tbl = document.getElementById('my-table').getElementsByTagName('tbody');
-  console.log(tbl);
+const addActionsSection = (data) => {
+  var tbl = document.getElementById('my-table');
+  var mainClosedDiv = document.createElement("div");
+  mainClosedDiv.setAttribute('class', 'actions-section');
+  mainClosedDiv.setAttribute('id', 'section-closed');
+  var div = document.createElement("div");
+  div.setAttribute('class', 'actions-column');
+  div.setAttribute('id', 'my-div');
+  for (var i = 0; i < data.length; i++) {
+      var innerDiv = document.createElement("div");
+      innerDiv.setAttribute('class', 'action');
+      innerDiv.innerHTML = getImage(data[i].ImageName);
+      div.appendChild(innerDiv);
+      mainClosedDiv.appendChild(div);
+      tbl.appendChild(mainClosedDiv);
+  }
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute('onClick', 'toggleActions');
+  newDiv.setAttribute('class', 'action');
+  newDiv.innerHTML = '<i class="fa fa-arrow-left"></i>';
+  mainClosedDiv.appendChild(newDiv);
+  tbl.appendChild(mainClosedDiv);
+};
+
+const getImage = (ImageName) => { 
+  switch (ImageName) {
+     case 'upload':
+         return '<i class="fa fa-upload"></i>';
+     case 'download':
+         return '<i class="fa fa-download"></i>';
+     case 'create_folder':
+         return '<i class="fa fa-folder-open"></i>';
+     case 'share':
+         return '<i class="fa fa-share-alt"></i>';
+     case 'tag':
+         return '<i class="fa fa-tag"></i>';
+     case 'cut':
+         return '<i class="fa fa-scissors"></i>';
+     case 'copy':
+         return '<i class="fa fa-files-o"></i>';
+     case 'paste':
+         return '<i class="fa fa-clipboard"></i>';
+     case 'delete':
+         return '<i class="fa fa-trash-o"></i>';
+     case 'search':
+         return '<i class="fa fa-search"></i>';
+     case 'folder_settings':
+         return '<i class="fa fa-file-code-o"></i>';
+     case 'send_email':
+         return '<i class="fa fa-paper-plane"></i>';
+     case 'send_link':
+         return '<i class="fa fa-link"></i>';
+     case 'history':
+         return '<i class="fa fa-history"></i>';
+     case 'refresh':    
+          return '<i class="fa fa-refresh"></i>';                                                    
+     default:
+         return '<i class="fa fa-arrow-left"></i>';     
+  }
+};
+
+const toggleActions = () => {
+
 };
 
 let table = document.querySelector('table');
@@ -92,6 +152,5 @@ fetch('/sample.json').then(res => res.json()).then((response) => {
   let data = Object.keys(tableData.GridData[0]);
   generateTable(table, tableData.GridData);
   generateTableHead(table, data); 
-  console.log(tableData.Actions); 
-  addColumn();
+  addActionsSection(tableData.Actions);
 });
